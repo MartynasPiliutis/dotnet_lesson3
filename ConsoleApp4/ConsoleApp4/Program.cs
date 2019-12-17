e7f8980200542b83d44c87859658b146f7684ed9
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-17 20:48:02 +0200

    L3P4 example1

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            Console.Write("Iveskite grupes pavadinima: ");
+            string grupesPavadinimas = Console.ReadLine();
+
+            Console.Write("Iveskite nariu skaiciu: ");
+            int nariuSkaicius = 0;
+
+            if (!int.TryParse(Console.ReadLine(), out nariuSkaicius))
+            {
+                Console.WriteLine("Ivesta neteisinga reiksme: Nariu skaicius");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Grupes pavadinimas: {0}", grupesPavadinimas);
+            Console.WriteLine("Grupes nariu skaicius: {0}", nariuSkaicius);
+            Console.ReadLine();
+                
+
         }
     }
 }
