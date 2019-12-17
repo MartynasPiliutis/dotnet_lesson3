a97e401803aba117b24cb528b323d7e18f5aabf9
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-17 20:03:08 +0200

    L3P3 task1 complete

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            //Task1
+            char letter1;
+            char letter2;
+            char letter3;
+            
+            Console.Write("Iveskite pirma simboli: ");
+            letter1 = Console.ReadKey().KeyChar;
+            Console.WriteLine("");
+            Console.Write("Iveskite antra simboli: ");
+            letter2 = Console.ReadKey().KeyChar;
+            Console.WriteLine("");
+            Console.Write("Iveskite trecia simboli: ");
+            letter3 = Console.ReadKey().KeyChar; ;
+            Console.WriteLine("");
+
+            Console.WriteLine("{2} {1} {0}", letter1, letter2, letter3);
+            Console.ReadLine();
+
+
         }
     }
 }
