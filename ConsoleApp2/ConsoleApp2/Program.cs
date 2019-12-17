80e22a8bc0947ecab14b75342df3924afb60fc10
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-17 19:23:14 +0200

    L3P2 example4 kintamuju reiksmiu konvertavimas

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            //Example4
+
+            double x = 1234.7;
+            int a;
+
+            //a = x; neveiks konvertavimas
+            a = (int)x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(a);
+
+            Console.ReadLine();
         }
     }
 }
